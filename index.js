2762ad6b200e1bfcc3cb02b76fe5b1ad3ca3c34e
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-08-10 13:56:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const config = require("./config.js");
 const db = require("croxydb")
 const client = new Client({
   partials: [
-    Partials.Message, // for message
-    Partials.Channel, // for text channel
-    Partials.GuildMember, // for guild member
-    Partials.Reaction, // for message reaction
-    Partials.GuildScheduledEvent, // for guild events
-    Partials.User, // for discord user
-    Partials.ThreadMember, // for thread member
+    Partials.Message, 
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Reaction,
+    Partials.GuildScheduledEvent, 
+    Partials.User, 
+    Partials.ThreadMember, 
   ],
   intents: [
-    GatewayIntentBits.Guilds, // for guild related things
-    GatewayIntentBits.GuildMembers, // for guild members related things
-    GatewayIntentBits.GuildBans, // for manage guild bans
-    GatewayIntentBits.GuildEmojisAndStickers, // for manage emojis and stickers
-    GatewayIntentBits.GuildIntegrations, // for discord Integrations
-    GatewayIntentBits.GuildWebhooks, // for discord webhooks
-    GatewayIntentBits.GuildInvites, // for guild invite managing
-    GatewayIntentBits.GuildVoiceStates, // for voice related things
-    GatewayIntentBits.GuildPresences, // for user presence things
-    GatewayIntentBits.GuildMessages, // for guild messages things
-    GatewayIntentBits.GuildMessageReactions, // for message reactions things
-    GatewayIntentBits.GuildMessageTyping, // for message typing things
-    GatewayIntentBits.DirectMessages, // for dm messages
-    GatewayIntentBits.DirectMessageReactions, // for dm message reaction
-    GatewayIntentBits.DirectMessageTyping, // for dm message typinh
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations, 
+    GatewayIntentBits.GuildWebhooks, 
+    GatewayIntentBits.GuildInvites, 
+    GatewayIntentBits.GuildVoiceStates, 
+    GatewayIntentBits.GuildPresences, 
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping, 
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions, 
+    GatewayIntentBits.DirectMessageTyping, 
     GatewayIntentBits.MessageContent, 
   ],
 });
